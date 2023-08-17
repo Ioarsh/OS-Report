bef8fc86d6abee6806f31f4bca1d7615e701f69d
Ioarsh <banks4247@gmail.com>
2023-08-17 16:09:13 +0700

    1.2.0

--- Report1_B/MemManagement.c
+++ Report1_B/MemManagement.c
@@ -5,6 +5,10 @@ int glo1,glo2,glo3;
 void call_stack(int depth){
     int stack_variable;
     printf("(Depth: %d)Address of stack_variable: %p\n",depth,&stack_variable);
+    /// uncomment to make stack not overflow
+    // if(depth>5){
+    //     return NULL;
+    // }
     call_stack(depth+1);
     
 }
