1343570ed2760bba9591356162fc853724b46168
Ioarsh <banks4247@gmail.com>
2023-08-20 19:57:38 +0700

    new

--- Report1_D/IOtime.py
+++ Report1_D/IOtime.py
@@ -3,24 +3,28 @@ import time
 
 #create sample.txt
 with open('sample.txt', 'w') as file:
-    for _ in range(1000):
+    for i in range(1000):
         file.write("Never gonna give you up.\n")
 
 # compute time
 start_time = time.time()
 total = 0
-for _ in range(10**7):
+for i in range(10**7):
     total += 1
 compute_time = time.time() - start_time
 
 # I/O time
 start_time = time.time()
-with open('sample.txt', 'r') as file:
-    for line in file:
-        time.sleep(0.001)  
+#Auto
+# with open('sample.txt', 'r') as file:
+#     for line in file:
+#         time.sleep(0.001)
+#USER manual
+input("Press Enter to continue...")  
 io_time = time.time() - start_time
 
-# Clean up the dummy file
+
+# Clean
 os.remove('sample.txt')
 
 print(f"Compute Time: {compute_time:.4f} seconds")
